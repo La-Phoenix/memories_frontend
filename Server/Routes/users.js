30d0df81a093a66f72264dc8fcba3a179ff74a41
samlaphoenix <samuelo@ithena.ai>
2023-12-30 05:00:46 +0100

    Changed Memories Repo

--- Server/Routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signIn, signUp } from "../Controllers/user.js";
-const Router = express.Router();
-
-Router.post("/signUp", signUp);
-Router.post("/signIn", signIn);
-
-export default Router;
